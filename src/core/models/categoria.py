e48efc690e5d6e3e35eaa8dcbf66da9232cbbbf2
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-05-03 20:01:29 -0300

    Modificacion en las relaciones

--- src/core/models/categoria.py
+++ src/core/models/categoria.py
@@ -1,7 +1,15 @@
 from src.core.models.database import db
 
+# class Categoria(db.Model):
+#     __tablename__="categorias"
+#     id = db.Column(db.Integer, primary_key=True, unique= True)
+#     nombre = db.Column(db.String(50), nullable=False, unique= True)
+#     publicaciones = db.relationship('Publicacion', backref='publicaciones', cascade='all, delete-orphan')
+    
 class Categoria(db.Model):
-    __tablename__="categorias"
-    id = db.Column(db.Integer, primary_key=True, unique= True)
-    nombre = db.Column(db.String(50), nullable=False, unique= True)
-    publicaciones = db.relationship('Publicacion', backref='publicaciones', cascade='all, delete-orphan')
+    __tablename__ = "categorias"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    nombre = db.Column(db.String(50), nullable=False, unique=True)
+    publicaciones = db.relationship('Publicacion', backref='categoria', cascade='all, delete-orphan')
+
+    
--- src/core/models/permiso.py
+++ src/core/models/permiso.py
@@ -1,7 +1,15 @@
 from src.core.models.database import db
 
+# class Permiso(db.Model):
+#     __tablename__="permisos"
+#     id = db.Column(db.Integer, primary_key=True, unique= True)
+#     nombre = db.Column(db.String(50), nullable=False, unique= True)
+#     permisos_rol = db.relationship('Permisos_Rol', backref='permisos_rol', cascade='all, delete-orphan')
+
+#el valor de backref debería ser un nombre que se usará para referirse a la relación desde el modelo relacionado. Aquí parece que estás usando el mismo nombre para el backref que para la relación en sí, lo que podría estar causando confusión o un conflicto.
+
 class Permiso(db.Model):
-    __tablename__="permisos"
-    id = db.Column(db.Integer, primary_key=True, unique= True)
-    nombre = db.Column(db.String(50), nullable=False, unique= True)
-    permisos_rol = db.relationship('Permisos_Rol', backref='permisos_rol', cascade='all, delete-orphan')
+    __tablename__ = "permisos"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    nombre = db.Column(db.String(50), nullable=False, unique=True)
+    permisos_roles = db.relationship('Permisos_Rol', backref='permiso', cascade='all, delete-orphan')
