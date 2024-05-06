0d653d1029ba0caf6bda868f6ad81d9f97ea9fd3
Lucas-Andres-GF <lucas.gallardo196304@alumnos.info.unlp.edu.ar>
2024-05-06 15:48:51 -0300

    Fix en publicaciones

--- src/core/models/publicacion.py
+++ src/core/models/publicacion.py
@@ -10,7 +10,7 @@ class Publicacion(db.Model):
     titulo = db.Column(db.String(255), nullable=False)
     descripcion = db.Column(db.String(255), nullable=False)
     foto_path = db.Column(db.String(255), nullable=False)  # Columna para almacenar la ruta de la imagen en el sistema de archivos
-    fecha_publicacion = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha_publicacion = db.Column(db.DateTime, default=datetime.now)
     filiales_horarios_dias = db.Column(db.String(255), nullable=False)
     id_categoria = db.Column(db.Integer, db.ForeignKey("categorias.id"), nullable=False)
     id_visibilidad = db.Column(db.Integer, db.ForeignKey("visibilidades.id"), nullable=False)
