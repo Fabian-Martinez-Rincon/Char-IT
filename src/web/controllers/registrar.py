78fae1e8839c4ff44d81f5cd57bfd30f577939c6
Fabian-Martinez-Rincon <fabianmartinezrincon.123@gmail.com>
2024-05-14 13:05:11 +0100

    Registrar Usuario Super Chequeado con la HU

--- src/web/controllers/registrar.py
+++ src/web/controllers/registrar.py
@@ -8,38 +8,49 @@ from src.web.controllers.root import index_get
 
 bp = Blueprint("registrar", __name__)
 
+from flask import render_template, redirect, url_for, flash, session
+from werkzeug.security import generate_password_hash
+from src.core.models.database import db
+from src.web.formularios.registrar import RegisterForm
+from src.core.models.usuario import Usuario
+from flask import Blueprint
+
+bp = Blueprint("registrar", __name__)
+
 @bp.route('/registrar_usuario', methods=['GET', 'POST'])
 def register():
-    if (session.get('user_id')):
-        flash('No puede realizar esta operación.', 'error')
-        return redirect(url_for('root.index_get'))
     form = RegisterForm()
     if form.validate_on_submit():
+        existing_user = Usuario.query.filter_by(email=form.email.data).first()
+        if existing_user:
+            flash('Ya existe un usuario registrado con ese mail', 'danger')
+            return redirect(url_for('registrar.register'))  # Asegúrate de que el nombre de la función es correcto
+        if len(form.password.data) < 8:
+            flash('La contraseña debe tener mínimo 8 caracteres', 'danger')
+            return render_template('owner/registrar.html', form=form)
+        
+        hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
+        new_user = Usuario(
+            nombre=form.nombre.data,
+            apellido=form.apellido.data,
+            email=form.email.data,
+            password=hashed_password,
+            dni=form.dni.data,
+            fecha_nacimiento=form.fecha_nacimiento.data,
+            telefono=form.telefono.data,
+            id_rol=1
+        )
         try:
-            existing_user = Usuario.query.filter_by(email=form.email.data).first()
-            existing_user2 = Usuario.query.filter_by(dni=form.dni.data).first()
-            if existing_user is None and existing_user2 is None:
-                hashed_password = generate_password_hash(form.password.data, method='pbkdf2:sha256')
-                new_user = Usuario(
-                    nombre=form.nombre.data,
-                    apellido=form.apellido.data,
-                    email=form.email.data,
-                    password=hashed_password,
-                    dni=form.dni.data,
-                    fecha_nacimiento=form.fecha_nacimiento.data,
-                    telefono=form.telefono.data,
-                    id_rol=1
-                )
-                db.session.add(new_user)
-                db.session.commit()
-                flash('Registration successful!', 'success')
-                print('no entiendo nada')
-                return redirect(url_for('root.index_get'))  # Asegúrate de que el nombre de la función es correcto
-            else:
-                print('El dni ya esta registraado :c')
-                flash('Email already registered.', 'danger')
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Fue registrado correctamente', 'success')
+            return redirect(url_for('root.login'))
         except Exception as e:
-            print(f"Se rompio todo {e}")
             db.session.rollback()
-            flash('Failed to register due to an error: {}'.format(e), 'danger')
+            flash(f'ups ocurrio un error: {e}', 'danger')
+    else:
+        for fieldName, errorMessages in form.errors.items():
+            for err in errorMessages:
+                flash(f'{err}', 'danger')
     return render_template('owner/registrar.html', form=form)
+
