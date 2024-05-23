92752cd448a0f9594254dd6b7b5d71a795b4602a
Lamella Luciana Elizabeth <luciana.lamella172870@alumnos.info.unlp.edu.ar>
2024-05-23 18:07:50 -0300

    Correccion BD y Registrar

--- src/core/models/oferta.py
+++ src/core/models/oferta.py
@@ -1,17 +1,20 @@
 from src.core.models.database import db
 from datetime import datetime
-from src.core.models.oferta import Oferta
 from src.core.models.publicacion import Publicacion
 from src.core.models.filial import Filial
 from src.core.models.estado import Estado
 
 class Oferta(db.Model):
-    __tablename__="ofertas"
-    id = db.Column(db.Integer, primary_key=True, unique= True)
+    __tablename__ = "ofertas"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     ofrecido = db.Column(db.Integer, db.ForeignKey("publicaciones.id"), nullable=False)
     solicitado = db.Column(db.Integer, db.ForeignKey("publicaciones.id"), nullable=False)
     fechaIntercambio = db.Column(db.Date, nullable=False)
     horaIntercambio = db.Column(db.Time, nullable=False)
     filial = db.Column(db.Integer, db.ForeignKey("filiales.id"), nullable=False)
     estado = db.Column(db.Integer, db.ForeignKey("estados.id"), nullable=False)
+    # Relación con Publicacion para la clave foránea 'ofrecido'
+    publicacion_ofrecido = db.relationship('Publicacion', foreign_keys=[ofrecido], backref='ofertas_ofrecidas')
+    # Relación con Publicacion para la clave foránea 'solicitado'
+    publicacion_solicitado = db.relationship('Publicacion', foreign_keys=[solicitado], backref='ofertas_solicitadas')
     
--- src/web/formularios/registrar.py
+++ src/web/formularios/registrar.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField, ValidationError
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Email
 from datetime import date
 
 def validate_fecha_nacimiento(form, field):
@@ -14,7 +14,11 @@ class RegisterForm(FlaskForm):
     nombre = StringField('Nombre', validators=[DataRequired(), Length(max=50)])
     apellido = StringField('Apellido', validators=[DataRequired(), Length(max=50)])
     password = PasswordField('Contraseña', validators=[DataRequired(), Length(max=50)])
-    email = StringField('Email', validators=[DataRequired(), Length(max=255)])
+    email = StringField('Email', validators=[
+        DataRequired(message="Este campo es obligatorio."),
+        Email(message="Por favor ingrese un correo electrónico válido.")
+    ])
+
     dni = StringField('DNI', validators=[Optional(), Length(min=8)])
     fecha_nacimiento = DateField('Fecha de Nacimiento', validators=[DataRequired(), validate_fecha_nacimiento], format='%Y-%m-%d')
     telefono = StringField('Teléfono', validators=[Optional(), Length(max=50)])
