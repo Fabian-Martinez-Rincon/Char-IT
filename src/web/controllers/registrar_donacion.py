03f4998bf59f5687101d19397eba8c40b82d0ce4
Austin <austin.myles192994@alumnos.info.unlp.edu.ar>
2024-06-26 18:05:52 -0300

    Fixes en el Registrar donación

--- src/web/controllers/registrar_donacion.py
+++ src/web/controllers/registrar_donacion.py
@@ -19,7 +19,7 @@ def registrar_donacion():
         return redirect(url_for('root.index_get'))
     if session.get('user_id'):
         rol = Usuario.query.get(session.get('user_id')).id_rol
-        if rol != 2:  
+        if rol == 1:  
                 flash('No tienes permiso para realizar esta operacion.', 'error')
                 return redirect(url_for('root.index_get'))
     form = RegistrarDonacionForm()
@@ -29,7 +29,7 @@ def registrar_donacion():
 def registrar_donacion_post():
     if session.get('user_id'):
         rol = Usuario.query.get(session.get('user_id')).id_rol
-        if rol != 2 :  
+        if rol == 1:  
             flash('No tienes permiso para realizar esta operacion.', 'error')
             return redirect(url_for('root.index_get'))
         
@@ -40,8 +40,8 @@ def registrar_donacion_post():
         telefono = form.telefono.data
         if (aux):
             telefono = Usuario.query.filter_by(email=email).first().telefono     
-        elif(telefono == ""):
-            flash('El campo telefono está vacio.', 'error')
+        elif not(aux) and not(telefono):
+            flash('El usuario no se encuentra registrado. Complete los campos como corresponde.', 'error')
             return redirect(url_for('registrar_donacion.registrar_donacion', form=form))
         
         monto = form.monto.data
