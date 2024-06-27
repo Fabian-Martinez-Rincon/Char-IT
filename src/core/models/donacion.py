8c97decc289d322fbeaa2820f80e0f45d821fec8
Lamella Luciana Elizabeth <luciana.lamella172870@alumnos.info.unlp.edu.ar>
2024-06-27 13:46:07 -0300

    Correcciones de la Base de datos

--- src/core/models/donacion.py
+++ src/core/models/donacion.py
@@ -8,6 +8,8 @@ class Donacion(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique= True)
     id_tipo = db.Column(db.Integer, db.ForeignKey("tipos.id"), nullable=False) # PUEDE SER PRODUCTO - EFECTIVO - TARJETA
     email = db.Column(db.String(255), nullable=False)
+    nombre = db.Column(db.String(50), nullable=False)
+    apellido = db.Column(db.String(50), nullable=False)
     telefono = db.Column(db.String(50), nullable=True) # OPCIONAL SI NO ESTA REGISTRADO
     descripcion = db.Column(db.String(255), nullable=True) # SI ES UN PRODUCTO VA EL NOMBRE DEL PRODUCTO, SI ES POR LA WEB VA EL NRO DE COMPROBANTE
     monto = db.Column(db.Float, nullable=True)
